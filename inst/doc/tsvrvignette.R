731acbe0485846be516a139bb758a0309b10022f
Daniel C. Reuman <reuman@ku.edu>
2021-01-09 01:30:05 +0000

    version 1.0.2

--- inst/doc/tsvrvignette.R
+++ inst/doc/tsvrvignette.R
@@ -1,11 +1,11 @@
-## ----JRG_data, echo=TRUE-------------------------------------------------
+## ----JRG_data, echo=TRUE------------------------------------------------------
 library(tsvr)
 class(JRGdat)
 names(JRGdat)
 d<-t(as.matrix(JRGdat[,2:dim(JRGdat)[2]]))
 dim(d)
 
-## ----vreq_classic_demo, echo=TRUE----------------------------------------
+## ----vreq_classic_demo, echo=TRUE---------------------------------------------
 res<-vreq_classic(d)
 class(res)
 names(res)
@@ -13,7 +13,7 @@ summary(res)
 print(res)
 all.equal(res$com,res$comnull*res$vr)
 
-## ----vreq_LdM_demo, echo=TRUE--------------------------------------------
+## ----vreq_LdM_demo, echo=TRUE-------------------------------------------------
 res<-vreq_LdM(d)
 class(res)
 names(res)
@@ -22,7 +22,7 @@ print(res)
 all.equal(res$com,res$comnull*res$vr)
 all.equal(res$com,vreq_classic(d)$com)
 
-## ----tsvreq_classic_demo, echo=TRUE--------------------------------------
+## ----tsvreq_classic_demo, echo=TRUE-------------------------------------------
 res<-tsvreq_classic(d)
 class(res)
 names(res)
@@ -32,17 +32,17 @@ all.equal(res$com,res$tsvr*res$comnull)
 all.equal(sum(res$com),vreq_classic(d)$com)
 all.equal(sum(res$comnull),vreq_classic(d)$comnull)
 
-## ----tsvreq_classic_demo_2, echo=TRUE------------------------------------
+## ----tsvreq_classic_demo_2, echo=TRUE-----------------------------------------
 sum(res$wts)
 res2<-vreq_classic(d)
 all.equal(sum(res$tsvr),res2$vr)
 all.equal(sum(res$wts*res$tsvr),res2$vr)
 
-## ----tsvreq_classic_demo_3, echo=TRUE, out.height="50%"------------------
+## ----tsvreq_classic_demo_3, echo=TRUE, out.height="50%"-----------------------
 plot(res,filename="Tsvreq_classic_demo")
 knitr::include_graphics("Tsvreq_classic_demo.pdf")
 
-## ----agg_demo, echo=TRUE-------------------------------------------------
+## ----agg_demo, echo=TRUE------------------------------------------------------
 res<-tsvreq_classic(d)
 aggresLong<-aggts(res,res$ts[res$ts>=4])
 aggresShort<-aggts(res,res$ts[res$ts<4])
